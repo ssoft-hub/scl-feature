17aff8c7650e6fc13292e3a2ab143d659052a646
ssoft <and.stepanov@gmail.com>
2024-08-01 21:36:00 +0300

    Fix syntax error

--- example/Atomic/main.cpp
+++ example/Atomic/main.cpp
@@ -8,35 +8,35 @@
 #include <ScL/Feature/Tool.h>
 #include <ScL/Utility/Qualifier.h>
 
-using Map = ::std::map< ::std::string, ::std::pair< ::std::string, int > >;
+using Map = ::std::map<::std::string, ::std::pair<::std::string, int>>;
 
-using AtomicMutexMap = ::ScL::Feature::Wrapper< Map, ::ScL::Feature::ThreadSafe::Atomic >;
-using RecursiveMutexMap = ::ScL::Feature::Wrapper< Map, ::ScL::Feature::ThreadSafe::RecursiveMutex >;
-using SharedMutexMap = ::ScL::Feature::Wrapper< Map, ::ScL::Feature::ThreadSafe::SharedMutex >;
+using AtomicMutexMap = ::ScL::Feature::Wrapper<Map, ::ScL::Feature::ThreadSafe::Atomic>;
+using RecursiveMutexMap = ::ScL::Feature::Wrapper<Map, ::ScL::Feature::ThreadSafe::RecursiveMutex>;
+using SharedMutexMap = ::ScL::Feature::Wrapper<Map, ::ScL::Feature::ThreadSafe::SharedMutex>;
 
-using DefaultMap = ::ScL::Feature::Wrapper< Map, ::ScL::Feature::Inplace::Default >;
-using ImplicitMap = ::ScL::Feature::Wrapper< Map, ::ScL::Feature::Implicit::Raw >;
+using DefaultMap = ::ScL::Feature::Wrapper<Map, ::ScL::Feature::Inplace::Default>;
+using ImplicitMap = ::ScL::Feature::Wrapper<Map, ::ScL::Feature::Implicit::Raw>;
 
-template < typename _MapType >
-void func ()
+template <typename _MapType>
+void func()
 {
     static _MapType test_map;
 
-    test_map[ "apple" ]->first = "fruit";
-    test_map[ "potato" ]->first = "vegetable";
+    test_map["apple"]->first = "fruit";
+    test_map["potato"]->first = "vegetable";
 
-    for ( size_t i = 0; i < 100000; ++i )
+    for (size_t i = 0; i < 100000; ++i)
     {
-        test_map->at( "apple" ).second++;
-        test_map->find( "potato" )->second.second++;
+        test_map->at("apple").second++;
+        test_map->find("potato")->second.second++;
     }
 
-    auto read_ptr = &::ScL::asConst( test_map );
+    auto read_ptr = &::ScL::asConst(test_map);
     ::std::cout
-        << "potato is " << read_ptr->at( "potato" ).first
-        << " " << read_ptr->at( "potato" ).second
-        << ", apple is " << read_ptr->at( "apple" ).first
-        << " " << read_ptr->at( "apple" ).second
+        << "potato is " << read_ptr->at("potato").first
+        << " " << read_ptr->at("potato").second
+        << ", apple is " << read_ptr->at("apple").first
+        << " " << read_ptr->at("apple").second
         << ::std::endl;
 }
 
@@ -46,37 +46,37 @@ void func ()
  * http://coliru.stacked-crooked.com/a/5def728917274b22
  *
  * Итоговые значения равные 100000 означают, что каждое сложение в каждом
- * из 10 потоков было выполнено потоко-безопасно.
+ * из 10 потоков было выполнено потокобезопасно.
  * Промежуточные значения не кратные 10000 говорят о том, что потоки исполнялись
  * параллельно или псевдопараллельно, т.е. прерывались посреди любой из операции
  * и в это время исполнялся другой поток.
  */
-template < typename _MapType >
-void example ()
+template <typename _MapType>
+void example()
 {
     ::std::cout << "Start" << ::std::endl;
     //::std::chrono::duration
 
-    ::std::vector< ::std::thread > threads( ::std::thread::hardware_concurrency() );
-    for ( auto & thread : threads )
-        thread = ::std::thread( func< _MapType > );
-    for ( auto & thread : threads )
+    ::std::vector<::std::thread> threads(::std::thread::hardware_concurrency());
+    for (auto &thread : threads)
+        thread = ::std::thread(func<_MapType>);
+    for (auto &thread : threads)
         thread.join();
 
     ::std::cout << "Finish" << ::std::endl;
 }
 
-int main ( int, char ** )
+int main(int, char **)
 {
-    // Паралельно, но не атомарно.
-    example< DefaultMap >();
-    // Паралельно, но не атомарно.
-    example< ImplicitMap >();
-    // Псевдопаралельно с блокировками, атомарно.
-    example< AtomicMutexMap >();
-    // Псевдопаралельно с блокировками, атомарно.
-    example< RecursiveMutexMap >();
-    // Псевдопаралельно с блокировками, атомарно.
-    example< SharedMutexMap >();
+    // Параллельно, но не атомарно.
+    example<DefaultMap>();
+    // Параллельно, но не атомарно.
+    example<ImplicitMap>();
+    // Псевдопараллельно с блокировками, атомарно.
+    example<AtomicMutexMap>();
+    // Псевдопараллельно с блокировками, атомарно.
+    example<RecursiveMutexMap>();
+    // Псевдопараллельно с блокировками, атомарно.
+    example<SharedMutexMap>();
     return 0;
 }
